6f0e43e1d3234a6940a7b9559c989257d15b59c9
Charles DuBose <kayakyakr@gmail.com>
2012-02-27 22:31:28 -0600

    making utilities not overwrite if apply already exists. updating readme to point to on-side API documentation

--- vendor/assets/javascripts/jslib/utilities.js
+++ vendor/assets/javascripts/jslib/utilities.js
@@ -1,26 +1,33 @@
-Object.apply = function(obj, cfg){
-  for(var i in cfg){
-    obj[i] = cfg[i];
-  }
-}
 
-Object.applyIf = function(obj, cfg){
-  for(var i in cfg){
-    if(obj[i] === undefined){
-      obj[i] = cfg[i];
+(function(){
+  if(!Object.apply){
+    Object.apply = function(obj, cfg){
+      for(var i in cfg){
+        obj[i] = cfg[i];
+      }
     }
   }
-}
-
-Array.prototype.shuffle = function() {
-    var tmp, current, top = this.length;
-
-    if(top) while(--top) {
-        current = Math.floor(Math.random() * (top + 1));
-        tmp = this[current];
-        this[current] = this[top];
-        this[top] = tmp;
+  
+  if(!Object.applyIf){
+    Object.applyIf = function(obj, cfg){
+      for(var i in cfg){
+        if(obj[i] === undefined){
+          obj[i] = cfg[i];
+        }
+      }
     }
-
-    return this;
-}
+  }
+  
+  Array.prototype.shuffle = function() {
+      var tmp, current, top = this.length;
+  
+      if(top) while(--top) {
+          current = Math.floor(Math.random() * (top + 1));
+          tmp = this[current];
+          this[current] = this[top];
+          this[top] = tmp;
+      }
+  
+      return this;
+  }
+})();
